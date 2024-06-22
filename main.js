27db6d03c7ce38677b996542a6e185bab173a539
Giacomo Radaelli <gradaellig@protonmail.com>
2024-06-22 17:09:09 +0200

    Changed the emoji search method, might improve it to search for similar words. I need to add more emoji->desc to my map, they're 28 now

--- main.js
+++ main.js
@@ -1,37 +1,77 @@
-let probability = 7  // 0-10, with 0 is never and 10 is always
+let probability = 7  // 0-10, with 0 is never and 10 is always. I know this is not implemented yet, but I will do it soon.
+
+async function loadEmojiMapping() {
+    const response = await fetch('./emojiMapping.json') // I hate this, but at the moment I don't have a better way to do it
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText)
+    }
+    
+    return await response.json()
+}
 
 async function modifyTextNodes(node, callback) {
     if (node.nodeType === Node.TEXT_NODE) {
         if (node.parentNode && node.parentNode.nodeName !== 'SCRIPT') {
-            node.textContent = await callback(node.textContent);
+            node.textContent = await callback(node.textContent)
         }
     } else {
         for (let child of node.childNodes) {
-            await modifyTextNodes(child, callback);
+            await modifyTextNodes(child, callback) // let's do it recursively
         }
     }
-}
-  
-async function stringAnalyzer(text) {
-    const words = text.split(" ");
-    const promises = words.map(analyzeAndReplaceWord);
-    const newWords = await Promise.all(promises);
-    return newWords.join(" ");
 }
 
-async function analyzeAndReplaceWord(word) {
-    let emoji = word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase();
-    try {
-        const response = await fetch("https://emoji-api.com/emojis?search=" + word + "&access_key=dd7d21a5d055d3c521c3dba1e38ba343e520734c"); // I need to change this to a better API, maybe a local database
-        const json = await response.json();
-        if (json.length > 0) {
-            emoji = json[0].character; // replace the word with the first found emoji
+function getEmoji(word, mapping) {
+    for (let i = 0; i < mapping.length; i++) {
+        if (find(word, mapping[i])) {
+            return mapping[word]
         }
-    } catch (error) {
-        console.error('Error:', error);
-		emoji = word;
     }
-    return emoji;
+
+    return mapping[word] || ""
+}
+
+function find(txt, src) {
+    var re = new RegExp(txt.split('').map(function(a,b,c){ return txt.substr(0, b)+a+'?.?'+ txt.substr(b+1);}).join('|'),'gi') // why does regex have to be so complicated?
+
+    return src.match(re) != null
+}
+
+function tokenize(input) {
+    return input.split(/\s+/g)
 }
   
-modifyTextNodes(document.body, stringAnalyzer);
+async function stringAnalyzer(text, mapping) {
+    var tokens = tokenize(text)
+
+    const promises = tokens.map(word => analyzeAndReplaceWord(word, mapping))
+    const newWords = await Promise.all(promises)
+
+    return newWords.join(" ")
+}
+
+async function analyzeAndReplaceWord(word, mapping) {
+    try {
+        const response = getEmoji(word.toLowerCase().replace(/[^a-z0-9_\s]/g, '').trim(), mapping)
+        if (response.length > 0) {
+            console.log('Replaced:', word, 'with:', response)
+            return response
+        } else {
+            emoji = word
+        }
+    } catch (error) {
+        console.error('Error:', error)
+    }
+
+    return word
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const wordToEmoji = await loadEmojiMapping()
+        await modifyTextNodes(document.body, text => stringAnalyzer(text, wordToEmoji))
+    } catch (error) {
+        console.error('Error during initialization:', error)
+    }
+});
